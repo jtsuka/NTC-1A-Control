58e5a5efd8662270f49ba2800b96913f480a5e57
jtsuka <jtsukan@gmail.com>
2025-06-17 08:25:59 +0900

    TC BYTE GAP Updata
    TC BYTE GAP Updata

--- ArduinoNanoEvery/TC-Emurator/TC-Emurator.ino
+++ ArduinoNanoEvery/TC-Emurator/TC-Emurator.ino
@@ -23,6 +23,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define BB_BAUD 330
 #define BIT_DELAY (1000000UL / BB_BAUD)
 #define HALF_DELAY (BIT_DELAY / 2)
+#define BYTE_GAP_TIME 400
 
 uint8_t recv_buf[6];
 
@@ -79,6 +80,8 @@ bool receive_packet() {
 void send_packet(uint8_t *buf) {
   for (int i = 0; i < 6; i++) {
     send_bitbang_byte(buf[i]);
+    delayMicroseconds(BYTE_GAP_TIME);            // ★バイト間ギャップ
+
   }
 }
 
@@ -92,7 +95,7 @@ void send_bitbang_byte(uint8_t b) {
   }
 
   digitalWrite(BB_TX_PIN, HIGH);
-  delayMicroseconds(BIT_DELAY);
+  delayMicroseconds(BIT_DELAY * 2);
 }
 
 void display_packet(const char *label, uint8_t *buf) {
