8ecd0534f594cedec5747de38bd8a0fab921fd01
jtsuka <jtsukan@gmail.com>
2025-06-18 13:47:31 +0900

    HALF DELAY
    HALF DELAY

--- SeeeduinoNano/NTC-1A-MAIN-UART-Bitbanking-Trans/NTC-1A-MAIN-UART-Bitbanking-Trans.ino
+++ SeeeduinoNano/NTC-1A-MAIN-UART-Bitbanking-Trans/NTC-1A-MAIN-UART-Bitbanking-Trans.ino
@@ -28,7 +28,7 @@ unsigned long t_start = 0;
 #define BB_RX_PIN 3
 #define BB_BAUD   300
 #define BIT_DELAY   (1000000UL / BB_BAUD)
-#define HALF_DELAY  ((BIT_DELAY / 2) + 20)
+#define HALF_DELAY  ((BIT_DELAY / 2) + 40)
 //#define BYTE_GAP_US 800
 #define BYTE_GAP_US 1500
 #define STOPBIT_GAP_US 100
