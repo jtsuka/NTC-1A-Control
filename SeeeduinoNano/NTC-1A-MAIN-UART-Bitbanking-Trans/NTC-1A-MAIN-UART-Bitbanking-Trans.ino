db4e481a2cf022872e804e9df2e5db6683323f2b
jtsuka <jtsukan@gmail.com>
2025-06-18 14:14:11 +0900

    DEBUG
    DEBUG

--- SeeeduinoNano/NTC-1A-MAIN-UART-Bitbanking-Trans/NTC-1A-MAIN-UART-Bitbanking-Trans.ino
+++ SeeeduinoNano/NTC-1A-MAIN-UART-Bitbanking-Trans/NTC-1A-MAIN-UART-Bitbanking-Trans.ino
@@ -13,11 +13,11 @@
 #define DEBUG_ENABLED 1  // 1: 有効 / 0: 無効
 
 #if DEBUG_ENABLED
-  #define DEBUG_PRINT(x)    Serial.print(x)
-  #define DEBUG_PRINTLN(x)  Serial.println(x)
+  #define DEBUG_PRINT(...)    Serial.print(__VA_ARGS__)
+  #define DEBUG_PRINTLN(...)  Serial.println(__VA_ARGS__)
 #else
-  #define DEBUG_PRINT(x)
-  #define DEBUG_PRINTLN(x)
+  #define DEBUG_PRINT(...)
+  #define DEBUG_PRINTLN(...)
 #endif
 
 
