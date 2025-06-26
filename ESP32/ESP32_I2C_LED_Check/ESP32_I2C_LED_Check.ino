ba0b9f8472ee3418a246b384d7bfa85f9c972706
jtsuka <jtsukan@gmail.com>
2025-06-26 14:31:56 +0900

    Create ESP32 I2C LED check
    Create ESP32 I2C LED check

---
+++ ESP32/ESP32_I2C_LED_Check/ESP32_I2C_LED_Check.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(5, 4);  // SDA=5, SCL=4
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("I2C scanner. Scanning ...");
+  byte count = 0;
+
+  for (byte i = 1; i < 127; i++) {
+    Wire.beginTransmission(i);
+    if (Wire.endTransmission() == 0) {
+      Serial.print("Found address: ");
+      Serial.print(i, HEX);
+      Serial.println();
+      count++;
+      delay(5);
+    }
+  }
+  Serial.print("Done. Found ");
+  Serial.print(count, DEC);
+  Serial.println(" device(s).");
+}
+
+void loop() {}
