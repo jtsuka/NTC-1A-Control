cda38c42d5eab9c76e093691b71543725e35ee2f
jtsuka <jtsukan@gmail.com>
2025-06-27 13:50:33 +0900

    Update Serial Test
    Update Serial Test

--- Python/Test_Trance_Serial_test.py
+++ Python/Test_Trance_Serial_test.py
@@ -1,6 +1,9 @@
 import serial
 
-ser = serial.Serial("/dev/ttyAMA0", 9600)
+ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+ser.reset_input_buffer()
+
 while True:
-    data = ser.read(6)
-    print("Received:", " ".join(f"{b:02X}" for b in data))
+    if ser.in_waiting >= 6:
+        data = ser.read(6)
+        print("Received:", ' '.join(f'{b:02X}' for b in data))
