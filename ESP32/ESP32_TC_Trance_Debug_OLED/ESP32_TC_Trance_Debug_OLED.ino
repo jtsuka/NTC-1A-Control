9157f20bed7b402ef1db812f81ebd4a72661ca8e
Junichi Tsukada <jtsukan@gmail.com>
2025-06-28 21:21:00 +0900

    UPdate
    UPdate

--- ESP32/ESP32_TC_Trance_Debug_OLED/ESP32_TC_Trance_Debug_OLED.ino
+++ ESP32/ESP32_TC_Trance_Debug_OLED/ESP32_TC_Trance_Debug_OLED.ino
@@ -21,6 +21,7 @@
 #define OLED_RESET    -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+
 // OLEDログクラス（排他制御付き）
 class OLEDLogger {
 private:
@@ -71,9 +72,43 @@ HardwareSerial SerialTC(1);
 HardwareSerial SerialPI(2);
 OLEDLogger oled;
 
+// Debug for Test Packet
+const uint8_t test_packet_pi[PACKET_SIZE] = { 0x01, 0x06, 0x05, 0x00, 0x00, 0x0C };
+const uint8_t test_packet_tc[PACKET_SIZE] = { 0x02, 0x06, 0x07, 0x00, 0x00, 0x0F };
+
 SemaphoreHandle_t xStopFlagMutex;
 QueueHandle_t queue_pi_rx, queue_tc_rx;
 volatile bool stopFlag = false;
+
+// 擬似タスク
+void task_fake_pi_tx(void* pv) {
+  while (1) {
+    bool flag;
+    xSemaphoreTake(xStopFlagMutex, portMAX_DELAY);
+    flag = stopFlag;
+    xSemaphoreGive(xStopFlagMutex);
+    if (!flag && digitalRead(SAFE_MODE_PIN)) {
+      xQueueSend(queue_pi_rx, test_packet_pi, portMAX_DELAY);
+      oled.logLine(0, "[TEST->TC] test_packet_pi");
+    }
+    vTaskDelay(pdMS_TO_TICKS(5000));  // 5秒間隔
+  }
+}
+
+void task_fake_tc_tx(void* pv) {
+  while (1) {
+    bool flag;
+    xSemaphoreTake(xStopFlagMutex, portMAX_DELAY);
+    flag = stopFlag;
+    xSemaphoreGive(xStopFlagMutex);
+    if (!flag && digitalRead(SAFE_MODE_PIN)) {
+      xQueueSend(queue_tc_rx, test_packet_tc, portMAX_DELAY);
+      oled.logLine(2, "[TEST->PI] test_packet_tc");
+    }
+    vTaskDelay(pdMS_TO_TICKS(5000));  // 5秒間隔
+  }
+}
+
 
 // ===== チェックサム検証 =====
 bool isChecksumValid(const uint8_t* data) {
@@ -181,6 +216,11 @@ void setup() {
   xTaskCreatePinnedToCore(task_tc_tx, "tc_tx", 2048, NULL, 1, NULL, 0);
   xTaskCreatePinnedToCore(task_pi_tx, "pi_tx", 2048, NULL, 1, NULL, 0);
   Serial.println("=== UART Relay with OLED Ready ===");
+  // Test用擬似タスク
+#if 0
+  xTaskCreatePinnedToCore(task_fake_pi_tx, "fake_pi_tx", 2048, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(task_fake_tc_tx, "fake_tc_tx", 2048, NULL, 1, NULL, 0);
+#endif
 }
 
 void loop() {
