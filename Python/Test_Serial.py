bab231d8fc87465de597fb47076ea6d9de985091
jtsuka <jtsukan@gmail.com>
2025-07-01 11:05:28 +0900

    UPdate Serial Test
    UPdate Serial Test

--- Python/Test_Serial.py
+++ Python/Test_Serial.py
@@ -1,11 +1,12 @@
+# uart_rx_test.py
 import serial
 
-ser = serial.Serial('/dev/serial0', 9600, timeout=2)
-print("Listening on /dev/serial0 (9600bps)...")
+ser = serial.Serial('/dev/serial0', 9600, timeout=1)
+print("Listening on /dev/serial0...")
 
 while True:
-    data = ser.read(6)
+    data = ser.read(6)  # 6バイト固定長パケット
     if data:
-        print("RX:", ' '.join(f"{b:02X}" for b in data))
+        print("Received:", ' '.join(f"{b:02X}" for b in data))
     else:
         print("[TIMEOUT] No data")
