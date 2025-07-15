d22815391fc1d0e88468c3cbb7a53579007a1999
jtsuka <jtsukan@gmail.com>
2025-07-15 13:13:08 +0900

    UPdate
    UPdate

--- ESP32/ESP32_Bitbang_Uart_Bridge/ESP32_Bitbang_Uart_Bridge.ino
+++ ESP32/ESP32_Bitbang_Uart_Bridge/ESP32_Bitbang_Uart_Bridge.ino
@@ -173,10 +173,10 @@ void TaskBitBangReceive(void *pvParameters) {
   while (1) {
     int len = bitBangReceivePacket(rxBuf, MAX_PACKET_LEN);
     // for Debug
-    ESP_EARLY_LOGI("BBRX", "len=%d", len);           // ★追加
+//    ESP_EARLY_LOGI("BBRX", "len=%d", len);           // ★追加
     if (len<=6) dbgCount[len]++;
-        if (len > 0) {
-        ESP_EARLY_LOGI("BBRX", "b0=%02X", rxBuf[0]); // ★追加
+    if (len > 0) {
+      ESP_EARLY_LOGI("BBRX", "b0=%02X", rxBuf[0]); // ★追加
     }
 
     if ((dbgCount[0]+dbgCount[6]) % 500 == 0) {   // 500回に1度だけ
