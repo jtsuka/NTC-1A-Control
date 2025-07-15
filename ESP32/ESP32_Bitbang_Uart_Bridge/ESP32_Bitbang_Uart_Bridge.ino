a96aeaf745ab2d53700a6eb5dd36b0c035b055b1
jtsuka <jtsukan@gmail.com>
2025-07-15 13:43:55 +0900

    FIx
    Fix

--- ESP32/ESP32_Bitbang_Uart_Bridge/ESP32_Bitbang_Uart_Bridge.ino
+++ ESP32/ESP32_Bitbang_Uart_Bridge/ESP32_Bitbang_Uart_Bridge.ino
@@ -178,7 +178,7 @@ void TaskBitBangReceive(void *pvParameters) {
     if (len<=6) dbgCount[len]++;
     if (len > 0) {
       ESP_EARLY_LOGI("BBRX", "b0=%02X", rxBuf[0]); // ★追加
-    } else if ( len = 0 && cnt < 3 ) {
+    } else if ( len = 0 && cnt++ < 3 ) {
       ESP_EARLY_LOGI("BBRX", "len=%d", len);           // ★追加
     }
 
