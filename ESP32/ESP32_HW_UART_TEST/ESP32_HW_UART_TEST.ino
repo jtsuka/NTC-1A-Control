314663b6f4001fb80d3380a9c93ed8eb69e79040
jtsuka <jtsukan@gmail.com>
2025-07-25 15:00:34 +0900

    Fix Cable name
    Fix Cable name

--- ESP32/ESP32_HW_UART_TEST/ESP32_HW_UART_TEST.ino
+++ ESP32/ESP32_HW_UART_TEST/ESP32_HW_UART_TEST.ino
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
 // UART 定義
-#define UART_PI_RX    44
-#define UART_PI_TX    43
-#define UART_TC_RX     3
-#define UART_TC_TX     2
+#define UART_PI_RX    44    // (黄)RX Pi->ESP32
+#define UART_PI_TX    43    // (白)TX RSP32->Pi
+#define UART_TC_RX     3    // (黄)RX TC->ESP32
+#define UART_TC_TX     2    // (白)TX ESP32->TC
 
 #define PACKET_LEN     6
 #define TIMEOUT_MS    100
@@ -13,10 +13,10 @@ HardwareSerial SerialPi(1); // UART1 ←→ Pi
 HardwareSerial SerialTC(2); // UART2 ←→ TC
 
 // LVC16T245 制御ピン
-#define PIN_OE1        5   // TX 出力有効 → 常時 LOW
-#define PIN_DIR1       6   // TX方向：ESP32 → TC → HIGH 固定
-#define PIN_OE2        7   // RX 出力有効 → 常時 LOW
-#define PIN_DIR2       8   // RX方向：TC → ESP32 → LOW 固定
+#define PIN_OE1        5   // (白)TX 出力有効 → 常時 LOW
+#define PIN_DIR1       6   // (黄)TX方向：ESP32 → TC → HIGH 固定
+#define PIN_OE2        7   // (白)RX 出力有効 → 常時 LOW
+#define PIN_DIR2       8   // (黄)RX方向：TC → ESP32 → LOW 固定
 
 void setup() {
   Serial.begin(115200);
