e080a1e1dae6e82896beb1724e9c1bd0288db753
jtsuka <jtsukan@gmail.com>
2026-05-19 10:04:24 +0900

    UPDATE RX_PIN 4

--- ESP32/ESP32S3_TC106_Snifar_V1.0/ESP32S3_TC106_Snifar_V1.0.ino
+++ ESP32/ESP32S3_TC106_Snifar_V1.0/ESP32S3_TC106_Snifar_V1.0.ino
@@ -26,7 +26,7 @@
 
 #include <Arduino.h>
 
-static const int RX_PIN = 44;             // 必要に応じて変更
+static const int RX_PIN = 4;             // 必要に応じて変更
 static const uint32_t BAUD_RATE = 300;
 static const uint32_t BIT_US = 1000000UL / BAUD_RATE;  // 約3333us
 
